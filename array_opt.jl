00843fa367ddd485da3aeca668e9a1e5b11efd64
Edwin O Ouko <eouko@mit.edu>
2024-11-11 21:00:27 -0500

    added comments for TODO tasks

--- array_opt.jl
+++ array_opt.jl
@@ -1,108 +1,115 @@
-using Hyperopt
-# using Enzyme
-using Optim
-using ForwardDiff
+using BlackBoxOptim
+using Plots
+using BayesianOptimization, GaussianProcesses, Distributions
+using Optim, Hyperopt
+using Random
+
+## TODO
+# - Add reference papers below
+# - Debug the optimization code below to get sensible outputs
+# - Differentiable Programming approaches in Julia
+# - 
+
+## Notes
+#  - Cost Reduction: Computational Realm
+# - If case study data is available => optimization approach would be more feasible
 
 # Temporary function: Returns the total heat output of all the wells given their locations
-function total_heat_output(px_py::Vector{Float64})
-   # px, py = px_py[1:2:end], px_py[2:2:end]
-   # len = max(length(px), length(py))
-   # well_production_heat = 1000 # J, assumed for now
-   # return - well_production_heat * len
-   return - (px_py[1]^2 - px_py[2]^2 + px_py[3]^2 - px_py[4]^2)
-end
-
-function combine_coords(px, py)
-   px_py = []
-   for (x, y) in zip(px, py)
-      push!(px_py, x)
-      push!(px_py, y)
-   end
-   return px_py
-end
-
-# Bayesian Optimization
-function bayesian_optimization(px, py, grid_size)
-   px_py = combine_coords(px, py)
-   len = length(px_py)
-   objective = function (resources, px_py)
-      res = Optim.optimize(total_heat_output, px_py, SimulatedAnnealing(), Optim.Options(time_limit=resources/100))
-      Optim.minimum(res), Optim.minimizer(res)
-   end
-   candidates = [LinRange(0, grid_size, Int(10*grid_size)) for _ in 1:len]
-   dims = [Hyperopt.Continuous() for _ in 1:len]
-   hohb = hyperband(objective, candidates, inner=BOHB(dims=dims); R=50, η=3, threads=true)
-   return hohb
+function f_heat(px_py)
+    px, py = px_py[1:2:end], px_py[2:2:end]
+    sum_heat = 0.0
+    for (i, p_x) in pairs(px)
+        p_y = py[i]
+        for (j, q_x) in pairs(px[i+1:end])
+            q_y = py[j]
+            sum_heat += sqrt((p_x - q_x)^2 + (p_y - q_y)^2)
+        end
+    end
+    return -sum_heat
 end
 
 
-function latin_hypercube_sampling(px, py, grid_size)
-   px_py = combine_coords(px, py)
-   len = length(px_py)
-   objective = function (resources, px_py)
-      res = Optim.optimize(total_heat_output, px_py, SimulatedAnnealing(), Optim.Options(time_limit=resources/100))
-      Optim.minimum(res), Optim.minimizer(res)
-   end
-   candidates = [LinRange(0, grid_size, 10*grid_size) for _ in 1:len]
-   hohb = hyperband(objective, candidates, inner=LHSampler(); R=50, η=3, threads=true)
-   return hohb
+function bayesian_optimization(px_py, grid_size)
+    # TODO: is a normal prior reasonable in this case
+    num_dims = length(px_py)
+    model = ElasticGPE( num_dims, 
+                        mean = MeanConst(grid_size/2.0), 
+                        kernel = SEArd(zeros(num_dims), 5.), 
+                        logNoise = 0.0, 
+                        capacity = 3000) 
+    # set_priors!(model.mean, [Normal(1, 2)]) # if not uniform prior (i.e Normal prior)
+    modeloptimizer = MAPGPOptimizer(every = 10, 
+                                    # noisebounds = [fill(-4, num_dims), fill(3, num_dims)], 
+                                    # kernbounds = [[-1, -1, 0], [4, 4, 10]], 
+                                    maxeval = 40)
+    opt = BOpt(f_heat, 
+                model, 
+                UpperConfidenceBound(), 
+                modeloptimizer, 
+                fill(0, num_dims), 
+                fill(grid_size, num_dims), 
+                repetitions = 5, 
+                maxiterations = 200, 
+                sense = Min, 
+                acquisitionoptions = (method = :LD_LBFGS, restarts = 5, maxtime = 0.1, maxeval = 1000), 
+                verbosity = Progress)
+    result = boptimize!(opt)
+    return result
+end
+ 
+## Latin Hypercube Sampling
+function latin_hypercube_sampling(f, points, grid_size)
+    if length(points) % 2 != 0
+        error("The points array must have an even number of elements representing coordinate pairs.")
+    end
+    # candidates = [collect(LinRange(0, grid_size, Int(10 * grid_size))) for _ in points] 
+    candidates = [LinRange(0, grid_size, Int(10 * grid_size)) for _ in points]  # TODO: run this to see if it resolves tha vlues-close-to-the-bordrr-problem
+    objective = function (resources, points)
+        lower = [0 for _ in 1:length(points)]
+        upper = [grid_size for _ in 1:length(points)]
+        res = Optim.optimize(f, lower, upper, points, Optim.Fminbox(Optim.GradientDescent()), Optim.Options(time_limit=resources/100))
+        Optim.minimum(res), Optim.minimizer(res)
+    end
+    hohb = hyperband(objective, candidates; R=50, η=3, threads=true)
+    return hohb
 end
 
-# Random Sampling
-function random_sampling(px, py, grid_size)
-   px_py = combine_coords(px, py)
-   len = length(px_py)
-   objective = function (resources, px_py)
-      res = Optim.optimize(total_heat_output, px_py, SimulatedAnnealing(), Optim.Options(time_limit=resources/100))
-      Optim.minimum(res), Optim.minimizer(res)
-   end
-   candidates = [LinRange(0, grid_size, 10*grid_size) for _ in 1:len]
-   hohb = hyperband(objective, candidates, inner=RandomSampler(); R=50, η=3, threads=true)
-   return hohb
+function plot_all(grid_size, num_wells)
+    num_dims = 2 * num_wells
+    points = [rand(0:grid_size) for _ in 1:num_dims]
+    # # Adaptive Differential Evolution Optimizer
+    adaptive_diff_evo_optim = bboptimize(f_heat; SearchRange = (0.0, grid_size), NumDimensions = num_dims, MaxTime = 30.0)
+    x1 = best_candidate(adaptive_diff_evo_optim)[1:2:end]
+    y1 = best_candidate(adaptive_diff_evo_optim)[2:2:end]
+
+    # # Random Search
+    rand_search = bboptimize(f_heat; SearchRange = (0.0, grid_size), NumDimensions = num_dims, Method=:random_search, MaxTime = 30.0)
+    x2 = best_candidate(rand_search)[1:2:end]
+    y2 = best_candidate(rand_search)[2:2:end]
+
+    # Bayesian Optimization
+    bayesian_optim = bayesian_optimization(fill(0.0, num_dims), Int(grid_size))
+    x3 = bayesian_optim.observed_optimizer[1:2:end]
+    y3 = bayesian_optim.observed_optimizer[2:2:end]
+
+    # Latin Hypercube Sampling with Gradient Descent
+    lhs = latin_hypercube_sampling(f_heat, points, grid_size).minimizer
+    x4 = lhs[1:2:end]
+    y4 = lhs[2:2:end]
+
+    ## Plotting
+    plot(
+    plot(x1, y1, label="Adaptive Differential Evolution Optimizer", xlabel="x", ylabel="y", seriestype=:scatter, marker=:circle),
+    plot(x2, y2, label="Random Search", seriestype=:scatter, marker=:star),
+    plot(x3, y3, label="Bayesian Optimization", seriestype=:scatter, marker=:cross),
+    plot(x4, y4, label="Latin Hypercube Sampling with GD", seriestype=:scatter, marker=:x),
+    layout=(2, 2), 
+    title="Well Locations"
+    )
+    plot!(legend=:outerbottom)
+    savefig("myplot.png") 
 end
 
-function combine_coords(px, py)
-   px_py = []
-   for (x, y) in zip(px, py)
-      push!(px_py, x)
-      push!(px_py, y)
-   end
-   return px_py
-end
-
-# Gradient-based optimization methods
-function gradient_based(f, px, py, grid_size, tol, max_iter, algorithm)
-   px_py = Vector{Float64}(combine_coords(px, py))
-   lower = [0.0 for _ in 1:length(px_py)]
-   upper = [grid_size for _ in 1:length(px_py)]
-   res = Optim.optimize(f, lower, upper, px_py, Fminbox(algorithm), Optim.Options(g_tol=tol, iterations=max_iter))
-   return Optim.minimizer(res)
-end
-
-# Naive Gradient Descent
-function gradient_descent(f, px, py, grid_size, tol, max_iter)
-   return gradient_based(f, px, py, grid_size, tol, max_iter, GradientDescent())
-end
-
-# ADAM
-function adam(f, px, py, grid_size, tol, max_iter)
-   # TODO: add box constraints instead of projecting the result. Cannot use FminBox with Adam yet in optim
-   # return gradient_based(f, px, py, grid_size, tol, max_iter, Adam())
-   px_py = Vector{Float64}(combine_coords(px, py))
-   res = Optim.optimize(f, px_py, Optim.Adam(epsilon=0.001), Optim.Options(g_tol=tol, iterations=max_iter))
-   result = Optim.minimizer(res)
-   for i in 1:length(result)
-      result[i] = clamp(result[i], 0.0, grid_size)
-   end
-   return result
-end
-
-
-# bayesian_optimization([1, 2], [2, 1], 20)
-
-# adam(total_heat_output, [1.0, 2.0], [2.0, 1.0], 100.0, 0.001, 10000)
-# gradient_descent(total_heat_output, [1.0, 2.0], [2.0, 1.0], 100.0, 0.001, 10000)
-bayesian_optimization([1.0, 2.0], [2.0, 1.0], 100.0)
-
-
-
+grid_size = 100.0
+num_wells = 20
+plot_all(grid_size, num_wells)
