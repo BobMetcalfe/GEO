b7006cfbe09a18eb135189526e4e13f4ffc3fc68
Edwin Ouko <eouko@mit.edu>
2025-02-25 21:07:18 -0500

    refactored the code

--- simulate_well.jl
+++ simulate_well.jl
@@ -1,75 +1,56 @@
 using LinearAlgebra
 using .Threads
 
-################################################################################
+########
 # Diffusion convection equation for temperature in a pipe-in-pipe geometry
 #
 # Equation: ρ c ∂ϕ/∂t =  ∂(λ ∂ϕ/∂x)/∂x + ∂(λ ∂ϕ/∂y)/∂y + ∂(λ ∂ϕ/∂z)/∂z
 #                        -(ε vx ∂ϕ/∂x + ε vy ∂ϕ/∂y + ε vz ∂ϕ/∂z)
 #                        + S
+########
 
-# Physical parameters ##########################################################
+### Physical parameters ###
 
-# Maximum simulation time [s]
-sim_time = 2400 # 40 minutes
+sim_time = 240 # Maximum simulation time [s], 40 minutes
 
-# Earth surface temperature [K]
-ϕs = 293.15 # 20 °C
+ϕs = 293.15 # Earth surface temperature [K], 20 °C
 
-# Fluid Initial temperature [K]
-ϕf0 = 293.15 # 20 °C
+ϕf0 = 293.15 # Fluid Initial temperature [K], 20 °C
 
-# Rock: granite #########################################
-# Rock density [kg/m3]
-ρr = 2750
-# Rock specific heat [J/(kg K)]
-cr = 790
-# Rock thermal conductivity [W/(m K)]
-λr = 2.62
+## Rock: granite ##
+ρr = 2750 # Rock density [kg/m3]
+cr = 790 # Rock specific heat [J/(kg K)]
+λr = 2.62 # Rock thermal conductivity [W/(m K)]
 
-# Pipes: polyethylene ####################################
-# Inner pipe inside radius [m]
-r1 = 0.1
-# Inner pipe thickness [m]
-t1 = 0.01
-# Inner pipe height [m]
-h1 = 8.5
-# Outer pipe thickness [m]
-t2 = 0.01
-# Outer pipe height [m]
-h2 = 9
-# Porosity: ratio of liquid volume to the total volume
-ε = 1
-# Pipe density [kg/m3]
-ρp = 961
-# Pipe specific heat [J/(kg K)]
-cp = 2900
-# Pipe thermal conductivity [W/(m K)]
-λp = 0.54
+## Pipes: polyethylene ##
+r1 = 0.1 # Inner pipe inside radius [m]
+t1 = 0.01 # Inner pipe thickness [m]
+h1 = 8.5 # Inner pipe height [m]
+t2 = 0.01 # Outer pipe thickness [m]
+h2 = 9 # Outer pipe height [m]
+ε = 1 # Porosity: ratio of liquid volume to the total volume
+ρp = 961 # Pipe density [kg/m3]
+cp = 2900 # Pipe specific heat [J/(kg K)]
+λp = 0.54 # Pipe thermal conductivity [W/(m K)]
 
-# Fluid: water #########################################
-# Fluid density [kg/m3]
-ρf = 997
-# Fluid specific heat capacity [J/(kg K)]
-cf = 4184
-# Fluid thermal conductivity [W/(m K)]
-λf = 0.6
-# Flow speed [m/s]
-uf = 0.01
-# Fluid dynamic viscosity at 25 °C [Pa⋅s]
-μf = 0.00089 # 0.0005465 at 50 °C
+## Fluid: water ##
+ρf = 997 # Fluid density [kg/m3]
+cf = 4184 # Fluid specific heat capacity [J/(kg K)]
+λf = 0.6 # Fluid thermal conductivity [W/(m K)]
+uf = 0.01 # Flow speed [m/s]
+μf = 0.00089 # Fluid dynamic viscosity at 25 °C [Pa⋅s], 0.0005465 at 50 °C
 
 # See https://gchem.cm.utexas.edu/data/section2.php?target=heat-capacities.php
 #     https://en.wikipedia.org/wiki/High-density_polyethylene
 #     https://en.wikipedia.org/wiki/Numerical_solution_of_the_convection%E2%80%93diffusion_equation
 
 
-# Numerical parameters #########################################################
+## Numerical parameters ##
 
 # Geometry distances [m]
 xx = 1
 yy = 1
-zz = h2 + 1 
+zz = h2 + 5 # compared to the length of outer pipe
 
 # dx, dy, dz [m]
 dx = 0.01
@@ -77,7 +58,8 @@ dy = 0.01
 dz = 0.125
 
 # Solve eq. system
-function updateϕ_domain!(ϕ2, ϕ1, d, vx, vy, vz, dx, dy, dz, dt, xc, yc, r1, t1, r2, ε, ii, jj, kk)
+function updateϕ_domain!(ϕ2, ϕ1, d, vx, vy, vz, dx, dy, dz, dt, xc, yc, r1, t1, r2, ε, ii, jj, kk, source=0.0)
+    # source: the source term in the heat equation
     for k = 2:kk-1
         @threads for j = 2:jj-1
             for i = 2:ii-1
@@ -105,8 +87,6 @@ function updateϕ_domain!(ϕ2, ϕ1, d, vx, vy, vz, dx, dy, dz, dt, xc, yc, r1, t
                         + ((d[i, j, k+1] + d[i, j, k]) / 2 * (ϕ1[i, j, k+1] - ϕ1[i, j, k]) / dz
                            -
                            (d[i, j, k] + d[i, j, k-1]) / 2 * (ϕ1[i, j, k] - ϕ1[i, j, k-1]) / dz) / dz)
-                # Source term
-                source = 0.0
                 # Update temperature
                 ϕ2[i, j, k] = (diff - conv + source) * dt + ϕ1[i, j, k]
             end
@@ -124,7 +104,7 @@ function updateϕ_boundaries!(ϕ, ii, jj, kk, dx, dy, dz, ϕ0)
 end
 
 function init(ii, jj, kk, dx, dy, dz, r1, t1, r2, dr, dp, df, uf, ϕf0, ϕ1, ϕ2, ϕ0)
-    # Initial and boundary conditions #############################################
+    ## Initial and boundary conditions ##
     d = zeros(ii, jj, kk)
     vx = zeros(ii, jj, kk)
     vy = zeros(ii, jj, kk)
@@ -181,47 +161,29 @@ function run_well_simulation(sim_time, ϕs, ϕf0, ρr, cr, λr, r1, t1, h1, t2, 
 
     # Rock temperature as a function of depth [°C]
     ϕ0(d) = ϕs + 0.5 * d
-    vol1 = π * r1^2 * h1
-    r2 = sqrt((vol1 + π * (r1 + t1)^2 * h1) / (h1 * π)) # <= vol1 = vol2 = π*r2^2*h1-π*(r1+t1)^2*h1
+    r2 = sqrt((r1^2 + (r1 + t1)^2)) #  vol1 = vol2, NOTE: does not have to be this way
 
-    # Pipe diffusion coefficient
-    dp = λp / (ρp * cp)
+    dp = λp / (ρp * cp) # Pipe diffusion coefficient
 
-    # Fluid diffusion coefficient
-    df = λf / (ρf * cf)
+    df = λf / (ρf * cf)  # Fluid diffusion coefficient
 
     # Flow speed [m/s]
     vx0 = uf
     vy0 = 0
     vz0 = 0
 
-    # Characteristic linear dimension (diameter of the pipe) [m]
-    Lf = 2r1
-
-    # Reynolds number
-    Re = ρf * uf * Lf / μf
-
-    rx = 0:dx:xx
-    ry = 0:dy:yy
-    rz = 0:dz:zz
-
     # dt using stability condition (check this)
     dtd = (1 / (2 * maximum([dr, dp, df])) * (1 / dx^2 + 1 / dy^2 + 1 / dy^2)^-1)
     dtc = minimum([dx / norm(vx0), dy / norm(vy0), dz / norm(vz0)])
     dt = minimum([dtd, dtc])
-    #dt = dt/10
-    # println("dt:$dt")
 
-    # No. of time iterations
-    tt = round(Int, sim_time / dt)
-    # println("tt:$tt")
+    tt = round(Int, sim_time / dt) # No. of time iterations
 
     # No. of spatial domain nodes
     ii = round(Int, xx / dx)
     jj = round(Int, yy / dy)
     kk = round(Int, zz / dz)
     nn = ii * jj * kk
-    # println("ii:$ii, jj:$jj, kk:$kk. nn:$nn.")
 
     # No. of height nodes
     hh1 = round(Int, h1 / dz)
@@ -231,7 +193,7 @@ function run_well_simulation(sim_time, ϕs, ϕf0, ρr, cr, λr, r1, t1, h1, t2, 
     ϕ2 = ones(ii, jj, kk)
     ϕ1 = ones(ii, jj, kk)
 
-    # Initial and boundary conditions #############################################
+    ## Initial and boundary conditions ##
     d, vx, vy, vz, xc, yc = init(ii, jj, kk, dx, dy, dz, r1, t1, r2, dr, dp, df, uf, ϕf0, ϕ1, ϕ2, ϕ0)
 
     vels = Array{Float64}(undef, 3, ii, jj, kk)
@@ -240,7 +202,7 @@ function run_well_simulation(sim_time, ϕs, ϕf0, ρr, cr, λr, r1, t1, h1, t2, 
     vels[3, :, :, :] = vz
 
     cumulative_heat_extracted = 0
-    for t = 0:2:tt # run the simulation for that long
+    for t = 0:2:tt # run the simulation 
         # Update ϕ
         updateϕ_domain!(ϕ2, ϕ1, d, vx, vy, vz, dx, dy, dz, dt, xc, yc, r1, t1, r2, ε, ii, jj, kk)
         updateϕ_boundaries!(ϕ2, ii, jj, kk, dx, dy, dz, ϕ0)
@@ -249,11 +211,6 @@ function run_well_simulation(sim_time, ϕs, ϕf0, ρr, cr, λr, r1, t1, h1, t2, 
         updateϕ_domain!(ϕ1, ϕ2, d, vx, vy, vz, dx, dy, dz, dt, xc, yc, r1, t1, r2, ε, ii, jj, kk)
         updateϕ_boundaries!(ϕ1, ii, jj, kk, dx, dy, dz, ϕ0)
 
-        # Save ϕ
-        # if t % 10 == 0
-        #     println("Iteration:$t, time:$(round(t*dt,digits=2))s, bottom temp:$(round(ϕ2[ii÷2,jj÷2,kk-1],digits=4) - 273.15)°C")
-        # end
-        
         Δϕ = ϕ2[Int(floor((ii+1)/2)), Int(floor((jj+1)/2)), 1] - ϕf0
 
         cumulative_heat_extracted -= π * r1^2 * vz[Int(floor((ii+1)/2)), Int(floor((jj+1)/2)), 1] * 2 * ρf * cf * Δϕ
